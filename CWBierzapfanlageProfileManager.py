79d9d0e7d90f84a24e0a92c99b45aeac6bfe3435
Thiedze <git@thiems.eu>
2014-06-18 21:27:57 +0200

    If Configfile not exist -> create one

--- CWBierzapfanlageProfileManager.py
+++ CWBierzapfanlageProfileManager.py
@@ -8,6 +8,7 @@ Profile Manager. Dient zum Erstellen/Aendern/Loeschen/Speichern von Profilen.
 """
 
 import ConfigParser
+import os
 from CWBierzapfanlageConstants import CWConstants
 
 DEBUG = True
@@ -39,13 +40,19 @@ class CWProfileManager:
 	def changeLeftBorderIgnorValue(self, value):
 		self.CWConstants.left_border_ignor = int(value)
 
+	def openConfigFile(self):
+		if os.path.exists(self.CWConstants.configFilename):
+			return open(self.CWConstants.configFilename, "r+")
+		else:
+			return open(self.CWConstants.configFilename, "w")
+
 	#Speichern eines "section" (Profiles)
 	def saveSection(self, section):
 		if not self.configParser.has_section(str(section)):
 			if DEBUG == True:
 				print ('Profile Manager New: ' + section)
 			#Oeffnen der Profile-Datei
-			cfgfile = open(self.CWConstants.configFilename,'r+')
+			cfgfile = self.openConfigFile()
 			#Hinzufuegen einer neuen "section" (Profile)
 			self.configParser.add_section(str(section))
 			self.updateSection(str(section))
@@ -57,7 +64,7 @@ class CWProfileManager:
 	def deleteSection(self, section):
 		if DEBUG == True:
 			print ("Profile Manager Delete: " + section)
-		cfgfile = open(self.CWConstants.configFilename,'w')
+		cfgfile = self.openConfigFile()
 		self.configParser.remove_section(str(section))
 		self.configParser.write(cfgfile)
 		cfgfile.close()
@@ -65,7 +72,7 @@ class CWProfileManager:
 	#Updaten einer bereits vorhandene "section" (Profiles)
 	def updateSection(self, section):
 		#Oeffnen der Profile-Datei
-		cfgfile = open(self.CWConstants.configFilename,'r+')
+		cfgfile = self.openConfigFile()
 		if DEBUG == True:
 			print ("Profile Manager Update: " + section)
 
